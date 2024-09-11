03bafa934ce8b20de136a4f7864985e6c21a7b34
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-09-11 09:16:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const path = require("path");
 
+const app = express();
+
+app.use(express.static(path.join(__dirname,"public"));
+
+app.get("/",(req,res)=> {
+   res.sendFile(path.join(__dirname,"public","index.html"));
+});
+
+const port = process.env.PORT || 3000;
+port.listen(port => {
+   console.log("Server listen on "+port);
+});
